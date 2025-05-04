90e8de8ad3dc16dc942086419d8f9af1538a6081
kip2 <tqa1no4@gmail.com>
2025-05-04 14:16:44 +0900

    Ch2: no_std

--- chapter-2/build-and-run-uefi.sh
+++ chapter-2/build-and-run-uefi.sh
@@ -1,4 +1,10 @@
 #!/bin/bash/
 
+cd repo/wasabi/
+cargo build --target x86_64-unknown-uefi
+
+cd -
+
 cp repo/wasabi/target/x86_64-unknown-uefi/debug/wasabi.efi mnt/EFI/BOOT/BOOTX64.EFI
 qemu-system-x86_64 -bios third_party/ovmf/RELEASEX64_OVMF.fd
+
